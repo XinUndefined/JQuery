95af48dacbeb81ceb3c733c719f4b2e675d91fa8
XinUndfined <z26319491z@yahoo.com.tw>
2020-04-13 21:23:55 +0800

    輪播 ok

--- all.js
+++ all.js
@@ -1,28 +1,4 @@
-//一般
-var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  loop: false,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
 var swiper = new Swiper(".swiper-container", {
-  //自動
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
   //循環
   slidesPerView: 1,
   loop: true,
@@ -30,8 +6,12 @@ var swiper = new Swiper(".swiper-container", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+
+  //自動
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
   },
 });
